6e545006da4e9aa9be7aee8dc8272296fbd6c456
zornlemma <zhou7yuan@gmail.com>
2022-09-13 09:39:55 +0800

    add pong wasm

---
+++ pong-wasm/pong.js
@@ -0,0 +1,12 @@
+
+function buttonClick() {
+    FS.mkdir('/data')
+    FS.mount(IDBFS, {}, '/data');
+    FS.syncfs(true, function() {
+        name = document.getElementById("name").value;
+        document.getElementById("header").remove();
+        document.getElementById("name").remove();
+        document.getElementById("btn").remove();
+        Module.mainLoop(name);
+    })
+}
